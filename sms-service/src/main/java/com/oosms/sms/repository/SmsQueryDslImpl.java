040f5ae980b9af05d6df20e0bbe96775a806231f
Jisu-Shin <sjszzang3053@gmail.com>
2025-09-23 18:43:23 +0900

    chore: queryDsl 라이브러리 추가

--- sms-service/src/main/java/com/oosms/sms/repository/SmsQueryDslImpl.java
+++ sms-service/src/main/java/com/oosms/sms/repository/SmsQueryDslImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-import static com.oosms.sms.domain.sms.QSms.sms;
+import static com.oosms.sms.domain.QSms.sms;
 
 @Repository
 public class SmsQueryDslImpl implements SmsQueryDsl{
