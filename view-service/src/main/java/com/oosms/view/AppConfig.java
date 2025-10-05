c9fd967fbd1c4d2b3b7d178f3c66107302ba9562
Jisu-Shin <sjszzang3053@gmail.com>
2025-10-06 01:17:14 +0900

    refactor : 기존 view-service 프로젝트 코드를 oo-sms 프로젝트 모듈로 이동

--- view-service/src/main/java/com/oosms/view/AppConfig.java
+++ view-service/src/main/java/com/oosms/view/AppConfig.java
@@ -1,6 +1,5 @@
 package com.oosms.view;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
