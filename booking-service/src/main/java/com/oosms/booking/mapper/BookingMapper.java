add99b47b690e7edc0bca8efc05b34332b3b91ee
Jisu-Shin <sjszzang3053@gmail.com>
2025-10-06 23:11:30 +0900

    refactor : 매핑

--- booking-service/src/main/java/com/oosms/booking/mapper/BookingMapper.java
+++ booking-service/src/main/java/com/oosms/booking/mapper/BookingMapper.java
@@ -3,10 +3,14 @@ package com.oosms.booking.mapper;
 import com.oosms.booking.domain.Booking;
 import com.oosms.common.dto.BookingListResponseDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface BookingMapper {
 
+    @Mapping(target = "bookId", source = "id")
+    @Mapping(target = "itemName", source = "item.name")
+    @Mapping(target = "bookingStatus", source = "status")
     BookingListResponseDto toDto(Booking entity);
 
 }
