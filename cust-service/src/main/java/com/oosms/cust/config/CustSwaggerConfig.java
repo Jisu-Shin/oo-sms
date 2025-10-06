52c43a21debad28747b7a47935db76d06fe25f64
Jisu-Shin <sjszzang3053@gmail.com>
2025-10-06 23:10:56 +0900

    rename : 동일 이름의 빈 충돌나서 클래스명 변경

--- cust-service/src/main/java/com/oosms/cust/config/CustSwaggerConfig.java
+++ cust-service/src/main/java/com/oosms/cust/config/CustSwaggerConfig.java
@@ -1,11 +1,11 @@
-package com.oosms.booking.config;
+package com.oosms.cust.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SwaggerConfig implements WebMvcConfigurer {
+public class CustSwaggerConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080")  // Swagger UI
