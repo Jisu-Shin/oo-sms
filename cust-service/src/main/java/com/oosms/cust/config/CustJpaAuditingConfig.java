9a8fb58305e35d50057014fd81ffd257f6287771
Jisu-Shin <sjszzang3053@gmail.com>
2025-10-06 23:12:58 +0900

    refactor : jpa auditing은 root의 jpaAuditingConfig에서 설정 , 서브의 @EnableJpaAuditin 제거

---
+++ cust-service/src/main/java/com/oosms/cust/config/CustJpaAuditingConfig.java
@@ -0,0 +1,8 @@
+package com.oosms.cust.config;
+
+import org.springframework.context.annotation.Configuration;
+
+// JPA Auditing은 Root의 JpaAuditingConfig에서 설정됨
+@Configuration
+public class CustJpaAuditingConfig {
+}
---
+++ sms-service/src/main/java/com/oosms/sms/config/SmsJpaAuditingConfig.java
@@ -0,0 +1,8 @@
+package com.oosms.sms.config;
+
+import org.springframework.context.annotation.Configuration;
+
+// JPA Auditing은 Root의 JpaAuditingConfig에서 설정됨
+@Configuration
+public class SmsJpaAuditingConfig {
+}
