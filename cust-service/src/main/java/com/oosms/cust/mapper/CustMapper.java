f4d5895fd6d35033eb8f4c7ca58df0e6f1fe2224
Jisu-Shin <sjszzang3053@gmail.com>
2025-10-07 03:46:53 +0900

    refactor : 전화번호 포맷으로 출력하기

--- cust-service/src/main/java/com/oosms/cust/mapper/CustMapper.java
+++ cust-service/src/main/java/com/oosms/cust/mapper/CustMapper.java
@@ -5,12 +5,20 @@ import com.oosms.common.dto.CustSaveRequestDto;
 import com.oosms.cust.domain.Cust;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface CustMapper {
 
     // 엔티티 -> DTO
-    @Mapping(source = "smsConsentType.displayName", target = "consentType")
+    @Mapping(target = "phoneNumber", source = "phoneNumber", qualifiedByName = "formatPhoneNumber")
+    @Mapping(target = "consentType", source = "smsConsentType.displayName")
     CustListResponseDto toDto(Cust entity);
     Cust toEntity(CustSaveRequestDto dto);
+
+    @Named("formatPhoneNumber")
+    default String formatPhoneNumber(String phoneNumber) {
+        String regEx = "(\\d{3})(\\d{4})(\\d{4})";
+        return phoneNumber.replaceAll(regEx, "$1-$2-$3");
+    }
 }
