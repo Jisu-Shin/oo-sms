fa1342f79d1d395080eb623554f2d420ed2b863a
Jisu-Shin <sjszzang3053@gmail.com>
2025-10-10 14:54:09 +0900

    refactor : booking @valid 유효성 검사 메시징 처리

--- common/src/main/java/com/oosms/common/dto/BookingCreateRequestDto.java
+++ common/src/main/java/com/oosms/common/dto/BookingCreateRequestDto.java
@@ -1,5 +1,8 @@
 package com.oosms.common.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,8 +10,14 @@ import lombok.Setter;
 @Getter @Setter
 @NoArgsConstructor
 public class BookingCreateRequestDto {
-    private long custId;
-    private long itemId;
-    private int count;
+    @NotNull(message = "회원ID는 필수값 입니다.")
+    private Long custId;
+
+    @NotNull(message = "공연ID는 필수값 입니다.")
+    private Long itemId;
+
+    @NotNull(message = "예매 수량은 필수값 입니다.")
+    @Min(value = 1, message = "수량은 1 이상이어야 합니다")
+    private Integer count;
 
 }
