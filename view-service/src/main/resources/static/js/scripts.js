c65f266d98ebc6bf3ef10904bec0a02a5ec62476
Jisu-Shin <sjszzang3053@gmail.com>
2025-10-13 15:31:20 +0900

    feat : 템플릿, 변수 수정, 삭제 버튼 생성

--- view-service/src/main/resources/static/js/scripts.js
+++ view-service/src/main/resources/static/js/scripts.js
@@ -1,3 +1,13 @@
+ /* const 객체 선언 */
+// 템플릿 유형 텍스트와 실제 Value를 매핑하는 객체
+const smsTypeMap = {
+    "정보성 문자": "INFORMAITONAL",
+    "광고성 문자": "ADVERTISING",
+    "인증 문자": "VERIFICATION"
+    // 테이블에 표시되는 모든 유형을 여기에 추가해야 합니다.
+};
+/* 끝 */
+
 var main = {
     init : function () {
 
@@ -6,29 +16,8 @@ var main = {
         /* 끝 */
 
         var _this = this;
-        $('#btn-add').on('click', function () {
-            _this.add();
-        });
 
-        $('#btn-close').on('click', function() {
-            _this.closeModal();
-        });
-
-        $('#btn-addPhoneNumber').on('click', function () {
-            $('#div-addPhoneNumber').show();
-            $('#div-choiceCust').hide();
-        });
-
-        $('#btn-choiceCust').on('click', function () {
-            $('#div-addPhoneNumber').hide();
-            $('#div-choiceCust').show();
-//            console.log("고객 전체 조회 시작");
-            oper.ajax('GET','','/api/custs',callback.choiceCust);
-        });
-
-        $('#btn-send').on('click', function () {
-            _this.sendSms();
-        });
+        $('#btn-send').on('click', _this.sendSms);
 
         $('#phoneNumber').on('input', function() {
             $(this).val($(this).val().replace(/[^0-9]/g, "").replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3").replace(/\-{1,2}$/g, ""));
@@ -42,9 +31,7 @@ var main = {
             _this.insertPlaceholder(this);
         });
 
-        $('#selectItem').on('change', function() {
-            _this.searchbooking();
-        });
+        $('#selectItem').on('change', _this.searchbooking);
 
         $('#price').on("input", function() {
             let price = $(this).val();
@@ -64,6 +51,18 @@ var main = {
                 $(this).val('');
             }
         });
+
+        $('.btn-template-edit').on("click", function() {
+            _this.editTemplate(this);
+        });
+
+        $('#btn-template-cancel-edit').on("click", _this.cancelTemplateEdit);
+
+        $('.btn-variable-edit').on("click", function() {
+            _this.editVariable(this);
+        });
+
+        $('#btn-variable-cancel-edit').on("click", _this.cancelVariableEdit);
 
     },
 
@@ -118,10 +117,6 @@ var main = {
         oper.ajax("POST",data, '/api/sms/send', callback.sendSms);
     },
 
-    closeModal : function() {
-        window.location.href='/cust/findAll';
-    },
-
     cancelBooking : function (btn) {
         let bookingRow = $(btn).closest("tr");
         console.log(bookingRow);
@@ -150,9 +145,79 @@ var main = {
         } else {
             $("#custBody").empty();
         }
+    },
 
+    editTemplate : function (btn) {
+        // 1. 수정할 데이터 가져오기
+        console.log("row읽기..");
+        let selectedRow = $(btn).closest("tr");
+        console.log(selectedRow);
+
+        let templateContent = selectedRow.find("td").eq(1).text().trim();
+        let smsTypeText = selectedRow.find("td").eq(2).text().trim();
+
+        console.log(templateContent);
+        console.log(smsTypeText);
+
+        // 3. 텍스트를 실제 Value로 변환
+        const smsTypeValue = smsTypeMap[smsTypeText];
+
+        // 2. 입력 필드에 값 세팅
+        $('#templateContent').val(templateContent);
+        $('#smsType').val(smsTypeValue);
+
+        // 3. 기존 버튼 숨기기 및 신규 버튼 추가
+        $('#template-register-group').addClass('d-none');
+        $('#template-edit-group').removeClass('d-none');
+    },
+
+    cancelTemplateEdit : function () {
+        console.log("취소할게요");
+
+        // 필드 값 초기화
+        $('#templateContent').val('');
+        $('#smsType').val('');
+
+        // 기존 버튼 숨기기 및 신규 버튼 추가
+        $('#template-register-group').removeClass('d-none');
+        $('#template-edit-group').addClass('d-none');
+    },
+
+    editVariable : function (btn) {
+        // 1. 수정할 데이터 가져오기
+        console.log("row읽기..");
+        let selectedRow = $(btn).closest("tr");
+        console.log(selectedRow);
+
+        let type = selectedRow.find("td").eq(0).text().trim();
+        let koText = selectedRow.find("td").eq(1).text().trim();
+        let enText = selectedRow.find("td").eq(2).text().trim();
+
+        console.log(type);
+        console.log(koText);
+
+        // 2. 입력 필드에 값 세팅
+        $('#variableType').val(type);
+        $('#koText').val(koText);
+        $('#enText').val(enText);
+
+        // 3. 기존 버튼 숨기기 및 신규 버튼 추가
+        $('#variable-register-group').addClass('d-none');
+        $('#variable-edit-group').removeClass('d-none');
+    },
+
+    cancelVariableEdit : function () {
+        console.log("취소할게요");
+
+        // 필드 값 초기화
+        $('#variableType').val("");
+        $('#koText').val("");
+        $('#enText').val("");
+
+        // 3. 기존 버튼 숨기기 및 신규 버튼 추가
+        $('#variable-register-group').removeClass('d-none');
+        $('#variable-edit-group').addClass('d-none');
     }
-
 };
 
 var callback = {
