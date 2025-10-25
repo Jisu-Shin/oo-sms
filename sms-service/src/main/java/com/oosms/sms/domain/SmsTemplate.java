9ea6b9112d553001bb30a36a7b378cf904f37ce7
Jisu-Shin <sjszzang3053@gmail.com>
2025-10-25 18:14:45 +0900

    refactor : audit 컬럼 추가

--- sms-service/src/main/java/com/oosms/sms/domain/SmsTemplate.java
+++ sms-service/src/main/java/com/oosms/sms/domain/SmsTemplate.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class SmsTemplate {
+public class SmsTemplate extends BaseTimeEntity {
 
     @Id
     @GeneratedValue
